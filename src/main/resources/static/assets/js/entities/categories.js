efc16f65a3d20a0dff55c6bc93e6cfecd915b1e4
tuannguyencorei7 <tuannguyencorei7@gmail.com>
2019-05-28 15:32:31 +0700

    dda

--- src/main/resources/static/assets/js/entities/categories.js
+++ src/main/resources/static/assets/js/entities/categories.js
@@ -7,35 +7,35 @@ $(function(){
         var $category = $(this);
         var categoryId = $category.val();
 
-        
+
         $.ajax({
-           
+
             type: "DELETE",
             url: "/categories/" + categoryId,
-            
+
             data: {},
-            
+
             success: function() {
-                
+
                 $('tr[data-category-id="' + categoryId + '"]').remove();
-                
+
                 $('#my-notice').addClass('my-notice-green').removeClass("my-no-line-height");
                 $('#my-notice i').addClass('far fa-smile');
                 $('#my-notice span:last-child').text("Category #" + categoryId + " has successfully been deleted.");
-                    
+
             },
-            
-            
+
+
             error: function() {
-            	
+
                 $('#my-notice').addClass('my-notice-red').removeClass("my-no-line-height");
                 $('#my-notice i').addClass('fas fa-dizzy');
                 $('#my-notice span:last-child').text("Something went wrong with deletion of category #" + categoryId);
-            
+
             }
-            
+
         });
-        
+
     });
 
 });
